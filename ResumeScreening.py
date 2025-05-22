2a33640f0cafed88b70decf4b8ba9e248a137610
DimuthK <dimuthnova2i@gmail.com>
2025-05-22 09:30:57 +0530

    punkt_tab change

--- ResumeScreening.py
+++ ResumeScreening.py
@@ -1,9 +1,9 @@
 import nltk
 
 try:
-    nltk.data.find('tokenizers/punkt')
+    nltk.data.find('tokenizers/punkt_tab')
 except LookupError:
-    nltk.download('punkt')
+    nltk.download('punkt_tab')
 
 try:
     nltk.data.find('corpora/stopwords')
